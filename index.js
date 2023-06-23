507b9e844b0c9bee6da95db13d807478ea278ab3
emmarupp <131177405+emmarupp@users.noreply.github.com>
2023-06-23 17:14:25 +0200

    Upload1

---
+++ index.js
@@ -0,0 +1,49 @@
+let leftClick = document.querySelector("#leftClickArrow");
+let rightClick = document.getElementById("rightClickArrow");
+let allPanels = document.getElementById("allPanels")
+console.log(leftClick);
+
+const clickArraySrc = [
+    "Hello1",
+    "Hello2",
+    "Hello3",
+    "Hello4",
+    "Hello5",
+    "Hello6",
+];
+const clickArrayTitle = [
+    "Hello1",
+    "Hello2",
+    "Hello3",
+    "Hello4",
+    "Hello5",
+    "Hello6",
+];
+const clickArrayHeadline = [
+    "Hello1",
+    "Hello2",
+    "Hello3",
+    "Hello4",
+    "Hello5",
+    "Hello6",
+];
+const clickArraySubline = [
+    "Hello1",
+    "Hello2",
+    "Hello3",
+    "Hello4",
+    "Hello5",
+    "Hello6",
+];
+
+let currentLeftPanel = 0;
+
+for (let i = 0; i < 5; i++) {
+    let completePanel = document.createElement("div");
+    let panelIMG = document.createElement("img");
+    let panelTexte = document.createElement("div");
+    let panelTitle = document.createElement("p");
+    let panelHeadline = document.createElement("p");
+    let panelSubline = document.createElement("p");
+}
+
